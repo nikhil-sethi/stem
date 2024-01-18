deaa478ae3faaa90c1db0bd6374a50ae19ebe77d
nikhl-sethi <sethi.nirvil@gmail.com>
2024-01-18 23:09:59 +0100

    Add base_interface + integrate keyboard teleop

---
+++ planner/scripts/planner/base_interface.py
@@ -0,0 +1,105 @@
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped, Quaternion
+from std_srvs.srv import Trigger, TriggerResponse, Empty
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+import time
+
+class BasePlanner:
+    def __init__(self, *args, **kwargs) -> None:
+        # self.active = True
+        self.end_mission = False
+
+    def target_pose(self) -> tuple:
+        """
+        This function returns a target pose. Needs to be overriden.
+
+        Return: x, y, z, yaw
+        """
+        raise NotImplementedError
+
+
+class BaseInterface:
+    """A base class to interface with drone toolbox and your 
+    custom Planner/controller implementation"""
+
+    def __init__(self, node_name="base_interface") -> None:
+        rospy.init_node(node_name, anonymous=True)
+
+        self.odom_sub = rospy.Subscriber("/mavros/local_position/odom", Odometry, callback = self.odom_callback)
+
+        self.pose_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
+        self.rate = rospy.Rate(50)  # 10 Hz
+
+        self.enable_ext_cont_server_ = rospy.Service("/px4_ext_cont_enable", Trigger, self.start_mission)
+        self.autopilot_ctrl = True # flag for when the toolbox is in control
+        self.ready = False  # goes from the planner side
+        self.mission_finished_client_ = rospy.ServiceProxy("/px4_mission_finished_ext_cont", Trigger);
+        self.reset_gazebo_client_ = rospy.ServiceProxy("/gazebo/reset_world", Empty);
+
+        self.planner = BasePlanner()
+        self.start_pose = [0, 0, 0.1, 0]
+
+    def sanity_checks(self):
+        if self.planner.end_mission:
+            res = self.mission_finished_client_.call()                            
+            self.autopilot_ctrl = res.success
+
+        # do some handy checks here to see if the planner is ready
+        self.ready = (not self.planner.end_mission)*(not self.autopilot_ctrl)
+
+        return self.ready
+
+    def start_mission(self, req:Trigger):
+        
+        self.autopilot_ctrl = False
+        response = TriggerResponse()
+        response.success = self.ready
+        if self.ready:
+            response.message = "Planner is ready. Sending control commands."
+        else:
+            response.message = "Planner not ready. Waiting..."
+        return response
+
+    def end_mission(self):
+        res = self.mission_finished_client_.call()                            
+        self.autopilot_ctrl = res.success
+
+    def odom_callback(self, msg):
+        self.position = [msg.pose.pose.position.x,msg.pose.pose.position.y,msg.pose.pose.position.z]
+        quat = [msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
+        self.orientation = euler_from_quaternion(quat)
+        # self.velocity = msg.twist.twist.linear_velocity
+
+    def send_pose_command(self, x, y, z, yaw_degrees):
+        pose_msg = PoseStamped()
+        pose_msg.header.frame_id = "base_link"
+        pose_msg.header.stamp = rospy.Time.now()
+        pose_msg.pose.position.x = x
+        pose_msg.pose.position.y = y
+        pose_msg.pose.position.z = z
+
+        # Convert yaw from degrees to quaternion
+        yaw_rad = yaw_degrees * (3.141592653589793 / 180.0)
+        quaternion = quaternion_from_euler(0, 0, yaw_rad)
+        pose_msg.pose.orientation = Quaternion(*quaternion)
+
+        self.pose_pub.publish(pose_msg)
+
+    def run(self):
+        while not rospy.is_shutdown() and not self.planner.end_mission:
+            self.sanity_checks()
+            if self.ready:
+                x, y, z, yaw = self.planner.target_pose()
+                self.send_pose_command(x, y, z, yaw)
+
+        
+            self.rate.sleep()
+        
+        rospy.loginfo("Exiting cleanly.")
+
+
+
+if __name__=="__main__":
+    base_interface = BaseInterface()
+    base_interface.run()
--- planner/src/drone_teleop.py
+++ planner/src/drone_teleop.py
@@ -7,7 +7,7 @@ from nav_msgs.msg import Odometry
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 import sys
 from std_srvs.srv import Trigger, TriggerResponse, Empty
-
+from planner.base_interface import BaseInterface, BasePlanner
 import sys, select, termios, tty
 
 
@@ -22,110 +22,60 @@ def getKey():
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
-class KeyboardController:
+class KeyboardPlanner(BasePlanner):
+    def __init__(self, *args, **kwargs):
+        self.pos_tol = 0.2
+        self.x, self.y, self.z,  self.yaw  = kwargs["start_pose"]
+        super().__init__()
+
+    def target_pose(self):
+        key = getKey()
+        # print(key)
+        if key == '+':  # thrist increase
+            self.z += self.pos_tol
+        elif key == '-': # thrust decrease
+            self.z += -self.pos_tol
+        # NUMPAD ON!
+        elif key == '4': # left
+            self.y += self.pos_tol
+        elif key == '6': # right
+            self.y += -self.pos_tol
+        elif key == '8': # forward
+            self.x += self.pos_tol
+        elif key == '2': # backward
+            self.x += -self.pos_tol
+        elif key == '7':
+            self.yaw += 10.0 # yaw ccw
+        elif key == '9':
+            self.yaw += -10.0 # yaw cw
+
+        elif key == 'q': # mission end
+            self.end_mission = True
+
+        elif key == 'r': # reset gazebo
+            res = self.reset_gazebo_client_.call()                            
+            
+        elif key == '\x03': # ctrl-c
+            self.end_mission = True
+
+        return self.x, self.y, self.z, self.yaw
+
+
+class TeleopInterface(BaseInterface):
     def __init__(self):
-        rospy.init_node('keyboard_controller', anonymous=True)
+        super().__init__("teleop_interface")
 
-        self.odom_sub = rospy.Subscriber("mavros/local_position/odom", Odometry, callback = self.odom_cb)
-
-        self.pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
-        self.rate = rospy.Rate(50)  # 10 Hz
-
-        self.enable_ext_cont_server_ = rospy.Service("/px4_ext_cont_enable", Trigger, self.start_mission)
-        self.autopilot_ctrl = True # flag for when the toolbox is in control
-        self.ready = False  # goes from the planner side
-        self.mission_finished_client_ = rospy.ServiceProxy("/px4_mission_finished_ext_cont", Trigger);
-        self.reset_gazebo_client_ = rospy.ServiceProxy("/gazebo/reset_world", Empty);
-
-        self.start_pos = [0,0,1]
-        self.position = [0,0,0]
-        self.orientation = [0,0,0]
-        self.key = None
+        self.planner = KeyboardPlanner(start_pose = self.start_pose)
 
     def sanity_checks(self):
-        # do some handy checks here to see if the planner is ready
-        self.ready = True
-
-    def start_mission(self, req:Trigger):
-        
-        self.autopilot_ctrl = False
-        response = TriggerResponse()
-        response.success = self.ready
-        if self.ready:
-            response.message = "Planner is ready. Sending control commands."
-        else:
-            response.message = "Planner not ready. Waiting..."
-        return response
-
-    def odom_cb(self, msg):
-        self.position = [msg.pose.pose.position.x,msg.pose.pose.position.y,msg.pose.pose.position.z]
-        quat = [msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
-        self.orientation = euler_from_quaternion(quat)
-        # self.velocity = msg.twist.twist.linear_velocity
-
-    def send_pose_command(self, x, y, z, yaw_degrees):
-        pose_msg = PoseStamped()
-        pose_msg.header = Header()
-        pose_msg.header.frame_id = "base_link"
-        pose_msg.header.stamp = rospy.Time.now()
-        pose_msg.pose.position.x = x
-        pose_msg.pose.position.y = y
-        pose_msg.pose.position.z = z
-
-        # Convert yaw from degrees to quaternion
-        yaw_rad = yaw_degrees * (3.141592653589793 / 180.0)
-        quaternion = quaternion_from_euler(0, 0, yaw_rad)
-        pose_msg.pose.orientation = Quaternion(*quaternion)
-
-        self.pub.publish(pose_msg)
-
-    def run(self):
-        x, y, z = self.start_pos
-        yaw = 0.0
-        tol = 0.2
-        while not rospy.is_shutdown():
-            self.sanity_checks()
-            if self.ready and not self.autopilot_ctrl:
-                key = getKey()
-                if key == '+':  # thrist increase
-                    z += tol
-                elif key == '-': # thrust decrease
-                    z += -tol
-                # NUMPAD ON!
-                elif key == '4': # left
-                    y += tol
-                elif key == '6': # right
-                    y += -tol
-                elif key == '8': # forward
-                    x += tol
-                elif key == '2': # backward
-                    x += -tol
-                elif key == '7':
-                    yaw += 10.0 # yaw ccw
-                elif key == '9':
-                    yaw += -10.0 # yaw cw
-
-                elif key == 'q': # mission end
-                    res = self.mission_finished_client_.call()                            
-                    self.autopilot_ctrl = res.success
-                
-                elif key == 'r': # reset gazebo
-                    res = self.reset_gazebo_client_.call()                            
-                    
-                elif key == '\x03': # ctrl-c
-                    break
-
-                self.send_pose_command(x, y, z, yaw)
-                self.rate.sleep()
-        
-        rospy.loginfo("Exiting cleanly.")
-
+        self.ready = super().sanity_checks() 
 
 if __name__ == '__main__':
-    controller = KeyboardController()
     settings = termios.tcgetattr(sys.stdin)
+    teleop_interface = TeleopInterface()
+    
     try:
-        controller.run()
+        teleop_interface.run()
     except Exception as e:
         print(e)
     finally:
