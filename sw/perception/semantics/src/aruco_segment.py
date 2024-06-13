beed230e2a6eb9ae5226957b70e7446b3c69254c
nikhil-sethi <sethi.nirvil@gmail.com>
2024-06-14 00:47:13 +0200

    Update priorities and marker models with  offline inference(#25)

--- sw/perception/semantics/src/aruco_segment.py
+++ sw/perception/semantics/src/aruco_segment.py
@@ -1,61 +1,57 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
-import cv2
+from semantics.segmentation_pipelines import ArUcoSegmenter
+from semantics.priorities import GTPriorityMap
 import numpy as np
 
-class ArUcoSegmenter:
-    def __init__(self):
+class PriorityMask:
+    def __init__(self) -> None:
         rospy.init_node('aruco_segmenter', anonymous=True)
+
+        # semantic segmentation pipeline (color -> labels)
+        self.segmenter = ArUcoSegmenter()
+
+        # priority inference
+        # self.priority_map = SemanticPriorityMap()
+        self.priority_map = GTPriorityMap("human", context="mine", p_max=8)
+
+        # ROS
         self.bridge = CvBridge()
-
-        # Subscribe to the RGB image topic
-        self.image_sub = rospy.Subscriber("/camera/color/image_raw/compressed", CompressedImage, self.image_callback)
-
-        # Publisher for the segmented image
-        self.segmented_image_pub = rospy.Publisher("/attention_map/2d/compressed", CompressedImage, queue_size=10)
+        self.rgb_image_sub = rospy.Subscriber("/camera/color/image_raw/compressed", CompressedImage, self.image_callback)
+        self.priority_mask_pub = rospy.Publisher("/priority_mask/compressed", CompressedImage, queue_size=10)
 
     def image_callback(self, msg):
         try:
             # Convert ROS Image message to OpenCV image
             header = msg.header
-            cv_image = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+            cv_image_bgr = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
         except CvBridgeError as e:
             rospy.logerr(e)
             return
 
-        # Convert image to grayscale
-        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        seg_img = self.segmenter.segment(cv_image_bgr)
+        # print(seg_img.max())
+        priority_mask_img = self.priority_map.priorities[seg_img] # pixelwise integer labels -> integer priorities
+        # print(np.unique(priority_mask_img))
+        self.publish_img(priority_mask_img*255/self.priority_map.p_max, header)
 
-        # Detect ArUco markers
-        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
-        parameters = cv2.aruco.DetectorParameters()
-        detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
-        corners, ids, _ = detector.detectMarkers(gray)
-        # Draw markers on the segmented image
-        segmented_image = np.zeros_like(gray)
-        
-        # print(corners)
-        if ids is not None:
-            # segmented_image = cv2.aruco.drawDetectedMarkers(segmented_image, corners)
-            for i in range(len(ids)):
-                corner = np.int0(corners[i]).reshape(-1, 2)
-                cv2.fillPoly(segmented_image, [corner], (int((ids[i])*255/5))) 
-            # print(ids)
 
-        # Publish the segmented image
+    def publish_img(self, img, header):
+         # Publish the segmented image
         try:
-            seg_msg = self.bridge.cv2_to_compressed_imgmsg(segmented_image)
+            seg_msg = self.bridge.cv2_to_compressed_imgmsg(img)
             seg_msg.header = header
             seg_msg.header.stamp = rospy.Time.now()
-            self.segmented_image_pub.publish(seg_msg)
+            self.priority_mask_pub.publish(seg_msg)
         except CvBridgeError as e:
             rospy.logerr(e)
 
+
 def main():
-    aruco_segmenter = ArUcoSegmenter()
+    priority_mask = PriorityMask()
     try:
         rospy.spin()
     except KeyboardInterrupt:
